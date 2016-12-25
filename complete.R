88db6f1ccf83fa3b9e3b87da8b32f42632157ca7
Tribhuwan Singh <tribhuwan50@gmail.com>
2016-12-25 07:03:33 +0530

    Update complete.R

--- complete.R
+++ complete.R
@@ -13,29 +13,46 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
-    
-    # set working directory
-    if(grep("specdata", directory) == 1) {
-        directory <- ("./specdata/")
-    }
-    # get the length of id vector
-    id_len <- length(id)
-    complete_data <- rep(0, id_len)
-    # find all files in the specdata folder
-    all_files <- as.character( list.files(directory) )
-    file_paths <- paste(directory, all_files, sep="")
-    j <- 1 
-    for (i in id) {
-        current_file <- read.csv(file_paths[i], header=T, sep=",")
-        complete_data[j] <- sum(complete.cases(current_file))
-        j <- j + 1
-    }
-    result <- data.frame(id = id, nobs = complete_data)
-    return(result)
-} 
+	m<-c()
+	as.numeric(m);
+	removeNA=TRUE
+	for(i in id) {
+		a<-i%/%10
+		if(a==0) {
+			id1<-paste(directory,"/00",i,".csv",sep="")
+			   }
+		else if((a>0)&&(a<10)) {
+			id1<-paste(directory,"/0",i,".csv",sep="")
+				}
+		else {
+			id1<-paste(directory,"/",i,".csv",sep="")
+			}
+			data<-read.csv(id1)
+			m<-c(m,sum(complete.cases(data)))
+			}
+	result<-data.frame(id=id,nobs=m)
+	return(result)
+}
 
 # tests
 complete("specdata", 1)
+##   id nobs
+## 1  1  117
 complete("specdata", c(2, 4, 8, 10, 12))
+##   id nobs
+## 1  2 1041
+## 2  4  474
+## 3  8  192
+## 4 10  148
+## 5 12   96
 complete("specdata", 30:25)
+##   id nobs
+## 1 30  932
+## 2 29  711
+## 3 28  475
+## 4 27  338
+## 5 26  586
+## 6 25  463
 complete("specdata", 3)
+##   id nobs
+## 1  3  243
